33a9d9371905b1d6ffae2b75046408b68b7e3e0f
James HN Sears <james.hn.sears@gmail.com>
2023-12-08 12:57:04 +0200

    assets

--- module/ui/zoom/src/main/kotlin/com/github/jameshnsears/chance/ui/zoom/ZoomComposable.kt
+++ module/ui/zoom/src/main/kotlin/com/github/jameshnsears/chance/ui/zoom/ZoomComposable.kt
@@ -1,13 +1,16 @@
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
@@ -53,16 +56,26 @@ fun ZoomColumn() {
                 colorFilter = ColorFilter.tint(Color.Red)
             )
 
-            Image(
-                painter = painterResource(R.drawable.d4_d8_d20),
-                contentDescription = null,
-                modifier = Modifier
-                    .padding(horizontal = 10.dp, vertical = 5.dp)
-                    .size(50.dp)
-                    .clickable {
-                        // Handle click
-                    }
-            )
+            Box(
+                contentAlignment = Alignment.Center
+            ) {
+                Image(
+                    painter = painterResource(R.drawable.d4_d8_d20),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .padding(horizontal = 10.dp, vertical = 5.dp)
+                        .size(50.dp)
+                        .clickable {
+                            // Handle click
+                        }
+                )
+
+                Text(
+                    text = "20",
+                    color = Color.Black,
+                    modifier = Modifier.padding(16.dp)
+                )
+            }
         }
     }
 
