ad621cf60240186a7c24168677a90c6aed11afbf
James HN Sears <james.hn.sears@gmail.com>
2023-12-09 13:10:12 +0200

    zoom layout

--- module/ui-zoom/src/main/kotlin/com/github/jameshnsears/chance/ui/zoom/ZoomComposable.kt
+++ module/ui-zoom/src/main/kotlin/com/github/jameshnsears/chance/ui/zoom/ZoomComposable.kt
@@ -3,6 +3,7 @@ package com.github.jameshnsears.chance.ui.zoom
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
@@ -15,13 +16,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
-import coil.ImageLoader
-import coil.compose.rememberAsyncImagePainter
-import coil.decode.SvgDecoder
 import com.github.jameshnsears.chance.data.R
+import com.github.jameshnsears.chance.data.domain.Dice
 
 @Composable
 fun ZoomColumn(viewModel: ZoomViewModel) {
@@ -55,19 +53,21 @@ fun ZoomColumn(viewModel: ZoomViewModel) {
                         showDiceIndex = false
                     }
 
-                    if (viewModel.imageDrawableAvailable(side)) {
-                        Image(
-                            painter = painterResource(id = side.imageDrawableId),
-                            contentDescription = null,
-                            modifier = Modifier
-                                .padding(horizontal = 5.dp, vertical = 5.dp)
-                                .size(viewModel.zoomSize())
-                                .clickable {
-                                    // Handle click
-                                }
-                        )
-                    } else {
-//                        SideImage(dice)
+                    Column {
+                        ImageSide(viewModel, dice)
+
+                        if (viewModel.imageDrawableAvailable(side)) {
+                            Image(
+                                painter = painterResource(id = side.imageDrawableId),
+                                contentDescription = null,
+                                modifier = Modifier
+                                    .padding(horizontal = 5.dp, vertical = 5.dp)
+                                    .size(viewModel.zoomSize())
+                                    .clickable {
+                                        // Handle click
+                                    }
+                            )
+                        }
                     }
                 }
             }
@@ -77,25 +77,27 @@ fun ZoomColumn(viewModel: ZoomViewModel) {
     }
 }
 
-//@Composable
-//fun SideImage(dice: Dice) {
-//    when (dice.sides.size) {
-//        2 -> {
-//
-//        }
-//    }
-//
-//    Image(
-//        painter = painterResource(id = side.imageDrawableId),
-//        contentDescription = null,
-//        modifier = Modifier
-//            .padding(horizontal = 5.dp, vertical = 5.dp)
-//            .size(viewModel.zoomSize())
-//            .clickable {
-//                // Handle click
-//            }
-//    )
-//}
+@Composable
+fun ImageSide(viewModel: ZoomViewModel, dice: Dice) {
+    val diceSideResourceId = when (dice.sides.size) {
+        2 -> R.drawable.d2
+        4, 8, 20 -> R.drawable.d4_d8_d20
+        6 -> R.drawable.d6
+        10 -> R.drawable.d10
+        else -> R.drawable.d4_d8_d20
+    }
+
+    Image(
+        painter = painterResource(id = diceSideResourceId),
+        contentDescription = null,
+        modifier = Modifier
+            .padding(horizontal = 5.dp, vertical = 5.dp)
+            .size(viewModel.zoomSize())
+            .clickable {
+                // Handle click
+            }
+    )
+}
 
 /*
 roll # : cumulative side #'s | dice side # | dice side # | ...
@@ -108,26 +110,3 @@ roll # : cumulative side #'s | dice side # | dice side # | ...
 
 -----------------
  */
-
-
-@Composable
-fun ZoomColumnWithCoilExample() {
-    // not usable with @Preview
-
-    val imageLoader = ImageLoader.Builder(LocalContext.current)
-        .components {
-            add(SvgDecoder.Factory())
-        }
-        .build()
-
-    Image(
-        painter = rememberAsyncImagePainter(R.drawable.d4_d8_d20, imageLoader),
-        contentDescription = null,
-        modifier = Modifier
-            .padding(horizontal = 10.dp, vertical = 5.dp)
-            .size(50.dp)
-            .clickable {
-                // Handle click
-            }
-    )
-}
