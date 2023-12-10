a6435766b40f0a629b29f40e3ec52ca92904cd47
James Sears <james.hn.sears@gmail.com>
2023-12-10 14:44:48 +0200

    zoop wip

--- module/data/src/main/kotlin/com/github/jameshnsears/chance/data/domain/Dice.kt
+++ module/data/src/main/kotlin/com/github/jameshnsears/chance/data/domain/Dice.kt
@@ -2,7 +2,7 @@ package com.github.jameshnsears.chance.data.domain
 
 data class Dice(
     var diceIndex: Int = 0,
-    var sides: List<Side> = (6 downTo 1).map { index -> Side(sideIndex = index) },
+    var sides: List<Side> = (2 downTo 1).map { index -> Side(sideIndex = index) },
     var colour: Colour = Colour(),
     var description: String = "",
     var descriptionStringsId: Int = 0,
