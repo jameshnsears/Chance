a1d010fa07eb5014c48a63723f3a9ada65108ce1
James Sears <james.hn.sears@gmail.com>
2024-01-07 14:14:59 +0200

    have to keep Zoom roll models!?
    Signed-off-by: James Sears <james.hn.sears@gmail.com>

--- module/test/src/test/kotlin/com/github/jameshnsears/chance/ui/zoom/bag/ZoomBagViewModelUnitTest.kt
+++ module/test/src/test/kotlin/com/github/jameshnsears/chance/ui/zoom/bag/ZoomBagViewModelUnitTest.kt
@@ -1,19 +1,15 @@
-package com.github.jameshnsears.chance.ui.zoom
+package com.github.jameshnsears.chance.ui.zoom.bag
 
-import com.github.jameshnsears.chance.data.bag.repository.BagRepositoryMock
-import com.github.jameshnsears.chance.data.bag.sample.BagSampleData
-import com.github.jameshnsears.chance.ui.zoom.bag.ZoomBagViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.UnconfinedTestDispatcher
 import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.runTest
 import kotlinx.coroutines.test.setMain
-import org.junit.Assert.assertEquals
 import org.junit.Assert.fail
 import org.junit.Test
 
-class ZoomViewModelUnitTest {
+class ZoomBagViewModelUnitTest {
     @OptIn(ExperimentalCoroutinesApi::class)
     @Test
     fun todo() = runTest {
--- module/ui-zoom/src/main/kotlin/com/github/jameshnsears/chance/ui/zoom/bag/ZoomBagViewModel.kt
+++ module/ui-zoom/src/main/kotlin/com/github/jameshnsears/chance/ui/zoom/bag/ZoomBagViewModel.kt
@@ -6,15 +6,6 @@ import com.github.jameshnsears.chance.ui.zoom.ZoomViewModel
 class ZoomBagViewModel(
     val bagRepository: BagRepositoryInterface
 ) : ZoomViewModel() {
-    //    var _description = MutableStateFlow(fetchInitialDescription())
-//    var description: StateFlow<String> = _description
-//
-//    fun fetchInitialDescription(): String = bagModel.fetchDiceDescription(diceIndex)
-//
-//    fun updateCurrentDescription(description: String) {
-//        Timber.d("description=", description)
-//        _description.value = description
-//    }
-    fun zoom() {
-    }
+    override fun zoom() {}
+
 }
