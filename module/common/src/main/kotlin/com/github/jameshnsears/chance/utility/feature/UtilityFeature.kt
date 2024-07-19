cf9102d25dc47d80027dcd9ff1cbd2e2f5054602
James Sears <james.hn.sears@gmail.com>
2024-07-19 17:51:15 +0300

    RepositoryBagImpl WIP

--- module/common/src/main/kotlin/com/github/jameshnsears/chance/utility/feature/UtilityFeature.kt
+++ module/common/src/main/kotlin/com/github/jameshnsears/chance/utility/feature/UtilityFeature.kt
@@ -9,7 +9,7 @@ sealed class UtilityFeature {
     companion object {
         private val enabled = setOf(
             Flag.NONE,
-            Flag.USE_PROTO_REPO
+//            Flag.USE_PROTO_REPO
         )
 
         fun isEnabled(flag: Flag): Boolean {
