594d983eeb9d5f6f5a22f0fc77f1618ae3499eb6
James HN Sears <james.hn.sears@gmail.com>
2026-05-19 09:48:10 +0300

    2.2.0 bump kotlin version

--- module/ui/src/test/kotlin/com/github/jameshnsears/chance/ui/zoom/bag/ZoomBagAndroidViewModelUnitTest.kt
+++ module/ui/src/test/kotlin/com/github/jameshnsears/chance/ui/zoom/bag/ZoomBagAndroidViewModelUnitTest.kt
@@ -10,8 +10,6 @@ import com.github.jameshnsears.chance.data.domain.core.roll.testdouble.RollHisto
 import io.mockk.spyk
 import kotlinx.coroutines.test.runTest
 import org.junit.Assert.assertEquals
-import org.junit.Assert.assertFalse
-import org.junit.Assert.assertTrue
 import org.junit.Before
 import org.junit.Test
 
